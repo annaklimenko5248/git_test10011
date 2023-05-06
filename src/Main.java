b274e7f4601f5f205beb876ca1a23913d7c3b2bf
annaklimenko <annaklimenko5248@gmail.com>
2023-05-06 13:59:14 +0300

    создали калк

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello");
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
     }
 }
